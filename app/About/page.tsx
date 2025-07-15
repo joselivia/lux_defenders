ef7b1ea5c97785fde4001105ff8080ccc5418bbb
Joselivia <josephmutunga39@gmail.com>
2025-07-15 10:10:40 +0300

    asdf

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -89,51 +89,6 @@ export default function TeamShowcase() {
 
   </div>
 </section>
-<section className="bg-[#002226] text-white p-6 md:px-10">
-  <div className="max-w-7xl mx-auto text-center">
-    <h2 className="text-3xl md:text-4xl font-bold text-cyan-400 mb-12">Meet Our Team</h2>
-
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-      {[
-        { name: "Joseph Mutunga", role: "CEO & Founder", image: "/logo.png",bio: "Visionary leader with a passion for digital defense and innovation." },
-        { name: "Jane Wanjiku", role: "Cybersecurity Analyst", image: "/team2.jpg", bio: "Expert in threat detection and incident response, ensuring robust security." },
-        { name: "Brian Otieno", role: "Penetration Tester", image: "/team3.jpg", bio: "Specializes in identifying vulnerabilities and strengthening defenses." },
-        { name: "Linda Mwikali", role: "Network Security Engineer", image: "/team4.jpg", bio: "Ensures secure network architecture and data protection." },
-        { name: "Kevin Kamau", role: "Cloud Security Specialist", image: "/team5.jpg", bio: "Expert in securing cloud environments and data integrity." },
-        { name: "Mercy Achieng", role: "Threat Intelligence Lead", image: "/team6.jpg", bio: "Analyzes cyber threats to provide actionable intelligence." },
-      ].map((member, index) => (
-      <div
-  key={index}
-  className="flex flex-col items-center text-center bg-[#00333a] rounded-xl p-4 hover:shadow-xl transition relative"
->
-  <div className="w-50 h-50 relative mb-4 group overflow-hidden rounded-xl border-4 border-cyan-400">
-    {/* Image */}
-    <Image
-      src={member.image}
-      alt={member.name}
-      fill
-      className="object-fit transition-transform duration-500 group-hover:scale-110"
-    />
-
- 
-    <div className="absolute inset-0 bg-black/80 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex flex-col justify-between p-4 rounded-2xl">
-      <p className="text-sm text-left">
-        {member.bio}
-      </p>
-      <div className="mt-2 text-right font-signature text-cyan-400">
-        — {member.name}
-      </div>
-    </div>
-  </div>
-
-  <h3 className="text-xl font-semibold">{member.name}</h3>
-  <p className="text-sm text-gray-400 mt-1">{member.role}</p>
-</div>
-
-      ))}
-    </div>
-  </div>
-</section>
 
 </div>
   
